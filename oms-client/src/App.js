d7d2f919e7af5cb9a0afff661c75dfae799bf017
UTKARSH YADAV <yadavutkarsh207@gmail.com>
2022-07-07 20:23:40 +0530

    add: pages, components, react-routes

--- oms-client/src/App.js
+++ oms-client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from './Pages/Home/Home';
+import Checkout from './Pages/Checkout/Checkout';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Products from './Pages/Products/Products';
+import Orders from "./Pages/Orders/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Products" element={<Products/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        </Routes>
+      </Router>
+      <Footer></Footer>
     </div>
   );
 }
