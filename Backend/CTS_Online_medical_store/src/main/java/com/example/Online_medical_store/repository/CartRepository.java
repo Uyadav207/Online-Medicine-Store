429c2ae464c13918fc9507491c3033dae01770af
unknown <sagarmish1234@gmail.com>
2022-07-09 16:44:08 +0530

    Everything done except login/signup

--- Backend/CTS_Online_medical_store/src/main/java/com/example/Online_medical_store/repository/CartRepository.java
+++ Backend/CTS_Online_medical_store/src/main/java/com/example/Online_medical_store/repository/CartRepository.java
@@ -2,19 +2,15 @@ package com.example.Online_medical_store.repository;
 
 import com.example.Online_medical_store.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    List<Cart> findByUserId(Long userId);
+    Optional<List<Cart>> findByUserId(Long userId);
 
     void deleteById(Long Id);
 
