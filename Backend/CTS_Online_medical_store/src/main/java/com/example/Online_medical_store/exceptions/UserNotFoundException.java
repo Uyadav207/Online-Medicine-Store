2898522fbdf28e9ae69e2481c35e976170fcd48d
UTKARSH YADAV <yadavutkarsh207@gmail.com>
2022-07-09 23:47:07 +0530

    Remove: Backend redundant folder remove

--- Backend/CTS_Online_medical_store/src/main/java/com/example/Online_medical_store/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Online_medical_store.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String msg){
-        super(msg);
-    }
-}
