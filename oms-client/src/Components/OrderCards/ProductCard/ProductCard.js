01e9023a780756d8518a7e3108746b8c8af68524
unknown <sagarmish1234@gmail.com>
2022-07-11 21:26:25 +0530

    Order page 96% done

--- oms-client/src/Components/OrderCards/ProductCard/ProductCard.js
+++ oms-client/src/Components/OrderCards/ProductCard/ProductCard.js
@@ -6,7 +6,7 @@ function ProductCard({id,productName,price, quantity, productExp, productMfd, im
     <div className='outer-product-container'>
         <div className='combine-image-details'>
         <div className='product-image'>
-            <p><img style={{ borderRadius: 10 }} src='https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg' width={60} height={60} alt="..."/></p>
+            <p><img style={{ borderRadius: 10 }} src={image} width={60} height={60} alt="..."/></p>
         </div>
         <div className='product-details'>
             <p className='p-d-n'>{productName}</p>
